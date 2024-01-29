a43275f230f80f7ff5d09fe473a7a80b95183c77
nobin-seemymachines <nobin@seemymachines.com>
2024-01-29 11:34:45 +0530

    developed

--- android/app/src/main/java/com/awesomeproject/MainActivity.kt
+++ android/app/src/main/java/com/awesomeproject/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.awesomeproject
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.zoontek.rnbootsplash.RNBootSplash;
+
 
 class MainActivity : ReactActivity() {
 
@@ -12,6 +15,12 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "AwesomeProject"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    RNBootSplash.init(this, R.style.BootTheme); 
+    super.onCreate(savedInstanceState); 
+   // super.onCreate(null)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- index.js
+++ index.js
@@ -3,7 +3,15 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
+import {store} from './src/redux/store';
+import {Provider} from 'react-redux';
 
-AppRegistry.registerComponent(appName, () => App);
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
